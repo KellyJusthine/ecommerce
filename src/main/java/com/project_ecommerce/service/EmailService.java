0a04ca7ac6f1c647a3fec2c29b05d0db38452925
KellyJusthine <kellyjusthine2003@gmail.com>
2025-07-21 20:43:32 -0300

    create EmailService

---
+++ src/main/java/com/project_ecommerce/service/EmailService.java
@@ -0,0 +1,43 @@
+package com.project_ecommerce.service;
+
+import com.project_ecommerce.exception.EmailFailureException;
+import com.project_ecommerce.model.VerificationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Value("${email.from}")
+    private String fromAddress;
+
+    @Value("${app.frontend.url}")
+    private String url;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    private SimpleMailMessage makeMailMessage() {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(fromAddress);
+        return simpleMailMessage;
+    }
+
+    public void sendVerificationEmail(VerificationToken verificationToken) {
+        SimpleMailMessage message = makeMailMessage();
+        message.setTo(verificationToken.getUser().getEmail());
+        message.setSubject("Verify your email to active your account.");
+        message.setText("Please follow the link below to verify your email to active your account. \n" +
+                url + "/auth/verify?token=" + verificationToken.getToken());
+        try {
+            javaMailSender.send(message);
+        } catch (MailException e) {
+            throw new EmailFailureException("FAILED");
+        }
+    }
+}
