4bf92bc823bf781a7095bdee97f8d3e4a3716ea9
KellyJusthine <kellyjusthine2003@gmail.com>
2025-07-21 20:44:35 -0300

    create UserNotVerifiedException

---
+++ src/main/java/com/project_ecommerce/exception/UserNotVerifiedException.java
@@ -0,0 +1,15 @@
+package com.project_ecommerce.exception;
+
+public class UserNotVerifiedException extends RuntimeException {
+
+    private boolean newEmailSent;
+
+    public UserNotVerifiedException(boolean newEmailSent) {
+        this.newEmailSent = newEmailSent;
+    }
+
+    public boolean isNewEmailSent() {
+        return newEmailSent;
+    }
+
+}
